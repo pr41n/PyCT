1e8986fbc2dbf0af3189a2fdd819bb6439634856
RobertJesus153 <robert.j.paneque@gmail.com>
2017-08-28 17:59:05 +0200

    Grammatical errors fixed

--- v0.4.6/Ajedrez-2/Teacher/Instructions/English.py
+++ v0.4.6/Ajedrez-2/Teacher/Instructions/English.py
@@ -1,14 +1,14 @@
 calibration_1 = "Click on the corners following the next instructions and \n" \
-                "since the point of view of the white player:\n\n" \
+                "from the point of view of the white player:\n\n" \
                 "     1- Square A1: left bottom corner\n" \
                 "     2- Square H1: right bottom corner\n" \
                 "     3- Square H8: right top corner\n" \
                 "     4- Square A8: left top corner\n\n" \
-                "When you will have finished, press enter and the rest\n" \
+                "When you have finished, press enter and the rest\n" \
                 "of the chessboard's points."
 
 
-calibration_2 = 'If you are agree with the points, press enter. Otherwise, escape.'
+calibration_2 = 'If you agree with the points, press enter. Otherwise, escape.'
 camera_choosing = 'Select the camera you are going to use.'
 
 player_1 = "Whites"
@@ -28,13 +28,13 @@ open_cv_1 = "Place the chessboard"
 open_cv_2 = "Calibrate"
 open_cv_3 = "On this window press:"
 
-sort_of_detection = "Choose the sort of detection you want"
+sort_of_detection = "Choose the type of detection you want"
 sort_of_detection_1 = "Manual"
 sort_of_detection_2 = "Semi-Automatic"
 sort_of_detection_3 = "Automatic"
-detection_description_1 = "The most accurate.\nYou must press space before and\nafter doing the play."
+detection_description_1 = "The most accurate one.\nYou must press space before and\nafter doing the play."
 detection_description_2 = "Less accurate than manual.\nYou must press space after\ndoing the play."
-detection_description_3 = "The less accurate.\nThe detection is making automatically\nafter doing the play."
+detection_description_3 = "The less accurate one.\nThe detection is making automatically\nafter doing the play."
 
 
 turn = "Turn"
