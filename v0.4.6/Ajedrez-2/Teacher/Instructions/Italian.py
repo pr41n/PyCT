fd0b3218c8659394b03a06a1f580edb827d5f5ec
RobertJesus153 <robert.j.paneque@gmail.com>
2017-09-01 19:28:53 +0200

    Italian translation

--- v0.4.6/Ajedrez-2/Teacher/Instructions/Italian.py
+++ v0.4.6/Ajedrez-2/Teacher/Instructions/Italian.py
@@ -1,45 +1,45 @@
 # -*- coding: utf-8 -*-
 
 
-calibration_1 = u"Marca los puntos siguiendo este orden y desde el punto\n" \
-                u"de vista del jugador blanco:\n\n" \
-                u"     1- Casilla A1: esquina inferior izquierda\n" \
-                u"     2- Casilla H1: esquina inferior derecha\n" \
-                u"     3- Casilla H8: esquina superior derecha\n" \
-                u"     4- Casilla A8: esquina superior izquierda\n\n" \
-                u"Mi recomendación es que marques las esquinas interiores en\n" \
-                u"vez de las líneas. Al acabar, pulsa enter y aparecerán el resto\n" \
-                u"de puntos del tablero."
+calibration_1 = u"Premi i punti seguendo quest'ordine e dal punto\n" \
+                u"di vista del giocatore bianco:\n\n" \
+                u"     1- Casella A1: angolo inferiore sinistro\n" \
+                u"     2- Casella H1: angolo inferiore destro\n" \
+                u"     3- Casella H8: angolo superiore destro\n" \
+                u"     4- Casella A8: angolo superiore sinistro\n\n" \
+                u"Il mio consiglio è che segnali gli angoli inferiori invece delle \n" \
+                u"linee. Una volta finito, premi enter e appariranno il resto\n" \
+                u"dei punti della scacchiera."
 
 
-calibration_2 = u'Si estás de acuerdo con los puntos, pulsa énter. Si no, escape'
-camera_choosing = u'Selecciona la cámara que vayas a usar.'
+calibration_2 = u"Se sei d'accordo con i punti, premi enter. Altrimenti, premi esc"
+camera_choosing = u'Seleziona la camera che vuoi usare.'
 
-player_1 = "Blancas"
-player_2 = "Negras"
-record = "Salvar partida"
-start = "Empezar"
-out = "Salir"
+player_1 = "Bianchi"
+player_2 = "Neri"
+record = "Salvare partita"
+start = "Iniziare"
+out = "Uscire"
 
-promote_1 = "Pieza en la que corona"
-promote_2 = "Pieza: "
+promote_1 = "Pezzo in quale corona"
+promote_2 = "Pezzo: "
 
-move_1 = "Escriba las casillas respectivas"
-move_2 = "Antes: "
-move_3 = "Ahora: "
+move_1 = "Scrivi le caselle rispettive"
+move_2 = "Prima: "
+move_3 = "Ora: "
 
-open_cv_1 = "Coloca el tablero"
-open_cv_2 = u"Calibración"
-open_cv_3 = "Sobre esta ventana pulsa:"
+open_cv_1 = "Posiziona la scacchiera"
+open_cv_2 = u"Calibrazione"
+open_cv_3 = "Premi su questa finestra:"
 
-sort_of_detection = u"Escoge el tipo de detección que prefieras"
-sort_of_detection_1 = "Manual"
-sort_of_detection_2 = u"Semi-Automática"
-sort_of_detection_3 = u"Automática"
-detection_description_1 = u"La más segura.\nDebes pulsar espacio antes y\ndespues de realizar la jugada."
-detection_description_2 = u"Menos segura que manual.\nDebes pulsar espacio despues\nde realizar la jugada."
-detection_description_3 = u"La menos segura.\nLa detección se realiza automáticamente\ndespues de realizar la jugada."
+sort_of_detection = u"Scegli il tipo di rilevazione che preferisci"
+sort_of_detection_1 = "Manuale"
+sort_of_detection_2 = u"Semi-Automatico"
+sort_of_detection_3 = u"Automatico"
+detection_description_1 = u"La più sicura.\nDevi premere spazio prima e\ndopo realizzare il movimento."
+detection_description_2 = u"Meno sicura che manuale.\nDevi premere spazio dopo aver\nrealizzato il movimento."
+detection_description_3 = u"La meno sicura.\nLa rivelazione si realizza automaticamente\ndopo aver realizzato il movimento."
 
 
 turn = "Turno"
-player = "Jugador"
+player = "Giocatore"
