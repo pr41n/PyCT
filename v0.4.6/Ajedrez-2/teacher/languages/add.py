cacc74e4f8d2d8b1f77fed5f05f1c447f3991bf7
RobertJesus153 <robert.j.paneque@gmail.com>
2017-10-12 11:22:33 +0200

    Template for including a new language

---
+++ v0.4.6/Ajedrez-2/teacher/languages/add.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+WARNING: Before doing anything, copy this file in the same directory
+and rename it with the name of the new language.
+
+Just complete the variable with the text you are going to write.
+You can use the other languages as a guide.
+If you are not going to write anything, leave a blank space.
+Once you have finished, load the program and your language will appear.
+"""
+
+babel = ' '     # Langague id in lowercase
+sound = ' '     # Langage spelled (f.e. spanish, english, ...). Must be lowercase
+name_of_the_button = ' '
+
+calibration = " "   # Instructions printed during the calibration
+
+# Players
+player_1 = " "
+player_2 = " "
+
+# Buttons
+record = " "
+start = " "
+out = " "
+
+# Promotion
+promote_1 = " "     # Title of the window
+promote_2 = " "     # Text before the entry
+
+# Manual movement
+move_1 = " "        # Title of the window
+move_2 = " "        # Text before the first entry
+move_3 = " "        # Text before the second entry
+
+# Pop-up windows
+open_cv_1 = " "     # Title of the first calibration window
+open_cv_2 = " "     # Title of the second calibration window
+open_cv_3 = " "     # Title of the window shown during the match
+
+# Window for choosing detection
+sort_of_detection = " " # Title of the window
+sort_of_detection_1 = " "   # Manual
+sort_of_detection_2 = " "   # Semi-Automatic
+sort_of_detection_3 = " "   # Automatic
+
+detection_description_1 = " "   # Manual description
+detection_description_2 = " "   # Semi-Automatic description
+detection_description_3 = " "   # Automatic description
+
+# Literal translations
+turn = " "
+player = " "
+pawn = " "
+rook = " "
+knight = " "
+bishop = " "
+queen = " "
+king = " "
+
+# Explanation of the moves
+incorrect_move_000 = " "    # First phrase written in each explanation
+incorrect_move_001 = " "    # Jump
+incorrect_move_002 = " "    # Pawns
+incorrect_move_003 = " "    # Rook
+incorrect_move_004 = " "    # Knight
+incorrect_move_005 = " "    # Bishop
+incorrect_move_006 = " "    # Queen
+incorrect_move_007 = " "    # King
+incorrect_move_008 = " "    # Castling
+
+# Audios
+aud_001 = " "   # Introduction
+aud_002 = " "   # Secong introduction (this will not appear)
+aud_003 = " "   # Before calibration
+aud_004 = " "   # Place the chessboard
+aud_005 = " "   # Move forward the rooks
+aud_006 = " "   # "Press enter when you have done it"
+aud_007 = " "   # Click on the corners
+aud_008 = " "   # "You can enlarge the window"
+aud_009 = " "   # Arduino connection
+aud_010 = " "   # No Arduino connection
+aud_011 = " "   # Order of pieces
+aud_012 = " "   # Column of the kings
+aud_013 = " "   # Who starts
+aud_014 = "%s %s"   # Eating, for example, Pawn by E5
+aud_015 = "%s %s"   # Movement, for example, Pawn to E5
+aud_016 = " "   # After viewing the points of the calibration
+aud_017 = " "   # Select the camera
+aud_018 = " "   # Select the type of detection
+aud_019 = " "   # Detection error
+aud_020 = " "   # Input manually the move
+aud_021 = " "   # Detection error and forced to input manually the move
+aud_022 = "%s %d"   # Checkmate, %s means the player who has won and %d the turns employed
+aud_023 = "%s"      # Name of the piece in which the pawn promotes
+aud_024 = ""    # Kingside castling
+aud_025 = " "   # Queenside castling
+aud_026 = " "   # Repeat the move
+aud_027 = " "   # Place again the pieces
+
+# Advices
+aud_A01 = " "   # Move less the pawns
+aud_A02 = " "   # Move more the pawns
+aud_A03 = " "   # Situate the knight around the center
