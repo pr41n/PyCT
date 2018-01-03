27e6d0fee1c2450fac34f77d0f8474b79116a44c
Unknown <robert.j.paneque@gmail.com>
2018-01-03 12:17:19 +0100

    Script synth merged with prevent_audio_error

--- v0.5.0/scripts/synth.py
+++ v0.5.0/scripts/synth.py
@@ -5,16 +5,25 @@ import pyttsx
 
 class Sts:
     def __init__(self, language):
+        self._unlocked = True
+
         self.engine = pyttsx.init()
         self.engine.setProperty('rate', 143)
         self.engine.setProperty('voice', language)
 
     def say(self, text):
         """This is the only way I found to make engine.say works"""
-        self.engine.say(text)
-        self.engine.say(' ')
-        self.engine.runAndWait()
-        self.engine.runAndWait()
+        while True:
+            if self._unlocked:
+                self.engine.say(text)
+                self.engine.say(' ')
+
+                self._unlocked = False
+                self.engine.runAndWait()
+                self.engine.runAndWait()
+                self._unlocked = True
+
+                return
 
 
 if __name__ == '__main__':
